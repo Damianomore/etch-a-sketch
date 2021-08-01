f300f7bbabd84544dbceb4a841dedd7fd501b9b0
Damian <pokemonmew34567@gmail.com>
2021-08-01 00:27:12 -0700

    Coloring Mode finalized. Beginning on special toggles

--- main.js
+++ main.js
@@ -1,11 +1,47 @@
 const gridContainer = document.querySelector('.grid-container');
 const gridSlider = document.querySelector('#gridSlider');
-let divList = undefined;
-let gridNum = 10;
+const preview = document.querySelector('#rgb-preview');
+const rLabel = document.querySelector('#red-label');
+const gLabel = document.querySelector('#green-label');
+const bLabel = document.querySelector('#blue-label');
+const sizeLabel = document.querySelector('#grid-size-label');
+const r = document.querySelector('#R');
+const g = document.querySelector('#G');
+const b = document.querySelector('#B');
+const rbBtn = document.querySelector('#rainbow-toggle');
+let rbToggle = false, erToggle = false, clToggle = false; 
+let rVal = 0, gVal = 0, bVal = 0;
 const isHover = e => e.querySelector(':hover') === e;    
+let divList = undefined;
+let gridNum = 0;
 
-gridContainer.style=`display: grid;grid-template-columns:repeat(${gridNum},auto);grid-template-rows: repeat(${gridNum},auto);`;
 
+rbBtn.addEventListener('click', () => {
+    if(rbToggle === true) {
+        rbToggle = false;
+        return;
+    }
+
+    rbToggle = true;
+})
+
+r.addEventListener('input', () => {
+    rVal = r.value;
+    preview.style.backgroundColor = `rgb(${rVal}, ${gVal}, ${bVal})`
+    rLabel.innerHTML = `Red: ${rVal}`;
+})
+g.addEventListener('input', () => {
+    gVal = g.value;
+    preview.style.backgroundColor = `rgb(${rVal}, ${gVal}, ${bVal})`;
+    gLabel.innerHTML = `Green: ${gVal}`;
+
+})
+b.addEventListener('input', () => {
+    bVal = b.value;
+    preview.style.backgroundColor = `rgb(${rVal}, ${gVal}, ${bVal})`;
+    bLabel.innerHTML = `Blue: ${bVal}`;
+
+})
 gridSlider.addEventListener('input', () => {
     gridNum = gridSlider.value;
     gridContainer.style=`display: grid;grid-template-columns:repeat(${gridNum},auto);grid-template-rows: repeat(${gridNum},auto);`;
@@ -14,21 +50,30 @@ gridSlider.addEventListener('input', () => {
     divList.forEach(grid => {
         grid.remove();
     });
+    sizeLabel.innerHTML = `Grid Size: ${gridNum}x${gridNum}`;
     initGrid();
 })
+
+
 function initGrid(){
     for (let index = 0; index < gridNum * gridNum; index++) {
         let newDiv = document.createElement("div");
         newDiv.className="gridDiv";
         let paddingNum = 40/gridNum;
-        newDiv.style = `outline: solid 1px rgba(0, 0, 0, 0.5);padding: ${paddingNum/2}rem;`;
+        newDiv.style = `padding: ${paddingNum/2}rem;`;
         newDiv.style.transition = "all 0.3s ease-in 0s";
 
         newDiv.addEventListener('mousemove', function checkHover() {
-            const hovered = isHover(newDiv);
-            if(hovered !== checkHover.hovered) {
-                newDiv.style.backgroundColor = "pink";
-                //checkHover.hovered = hovered;
+            if(rbToggle === true) {
+                console.log("set");
+                newDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+            }
+            else { 
+                const hovered = isHover(newDiv);
+                if(hovered !== checkHover.hovered) {
+                    newDiv.style.backgroundColor = `rgb(${rVal}, ${gVal}, ${bVal})`;
+                    //checkHover.hovered = hovered;
+                }
             }
         })
 
